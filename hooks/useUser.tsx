44020497c6322648e3ad4e20050a3e13bea0cc26
Dogukan Aktas <aktasdogukann@gmail.com>
2023-03-13 19:13:16 +0300

    initial commit

---
+++ hooks/useUser.tsx
@@ -0,0 +1,27 @@
+import useSWR from "swr";
+
+interface IUser {
+  age: number;
+  name: string;
+  surname: string;
+}
+
+const useUser = () => {
+  const fetcher = async (
+    input: RequestInfo,
+    init: RequestInit
+  ): Promise<IUser> => {
+    const response = await fetch(input, init);
+    return response.json();
+  };
+
+  const { data, error, isLoading } = useSWR("/api/user", fetcher);
+
+  return {
+    user: data,
+    isLoading,
+    isError: error,
+  };
+};
+
+export default useUser;
---
+++ pages/api/user.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+interface IResponseData {
+  name: string;
+  surname: string;
+  age: number;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseData>
+) {
+  res.status(200).json({ name: "dogukan", surname: "aktas", age: 31 });
+}
