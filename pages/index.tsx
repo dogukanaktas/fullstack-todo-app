b5763dc11af249375b3011fbc3209bd3340dbbb7
Dogukan Aktas <aktasdogukann@gmail.com>
2023-04-03 23:04:12 +0300

    feat: list todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,43 @@
 import type { NextPage } from 'next';
-import useUser from '../hooks/useUser';
+import { useEffect, useState } from 'react';
+
+const URL = 'http://localhost:8000/todos';
+
+interface Todo {
+  _id: string;
+  todo: string;
+  isCompleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
 
 const Home: NextPage = () => {
-  const { user } = useUser();
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  return <div>{user?.name}</div>;
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await fetch(URL);
+        const data: Todo[] = await response.json();
+        setTodos(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTodos();
+  }, []);
+
+  return (
+    <div>
+      <ol>
+        {todos.map((item) => (
+          <li key={item._id}>{item.todo}</li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Home;
