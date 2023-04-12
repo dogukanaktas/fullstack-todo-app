3b15bf1c9de3d5371dea87a359778cb3a6318879
Dogukan Aktas <aktasdogukann@gmail.com>
2023-04-12 22:22:40 +0300

    feat: add deleteAllTodos and deleteTodo helpers

--- lib/getData.tsx
+++ lib/getData.tsx
@@ -1,7 +1,9 @@
 import type { Todo } from '../types';
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const getTodos = async () => {
-  const URL = 'https://jsonplaceholder.typicode.com/todos/';
+  const URL = `${BASE_URL}/todos/`;
   let data: Todo[] = [];
   try {
     const response = await fetch(URL);
@@ -14,11 +16,42 @@ export const getTodos = async () => {
 };
 
 export const getTodo = async (id: number | string) => {
-  const URL = `https://jsonplaceholder.typicode.com/todos/${id}`;
+  const URL = `${BASE_URL}/todos/${id}`;
   let data: Todo = {} as Todo;
   try {
     const response = await fetch(URL);
     data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteAllTodos = async () => {
+  const URL = `${BASE_URL}/todos/`;
+  let data: Todo = {} as Todo;
+  try {
+    const response = await fetch(URL, {
+      method: 'DELETE',
+    });
+    data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTodo = async (id: number | string) => {
+  const URL = `${BASE_URL}/todos/${id}`;
+  console.log(BASE_URL);
+  let data: Todo = {} as Todo;
+  try {
+    const response = await fetch(URL, {
+      method: 'DELETE',
+    });
+    console.log('response', response);
 
     return data;
   } catch (error) {
